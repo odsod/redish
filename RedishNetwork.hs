401bd9eb5664f2bb7f4ae409e0c99a414997fbb9
Tobias <grobbe@Norom-Ubuntu.(none)>
2013-03-14 10:56:45 +0100

    cleaned up the cmd and network layers

--- RedishNetwork.hs
+++ RedishNetwork.hs
@@ -9,20 +9,17 @@ import RedishCmd
 defaultPort :: Int
 defaultPort = 7777
 
-
 handleConns :: Socket -> (TVar DB) -> IO ()
 handleConns s db = do
   (handle, host, port) <- accept s -- accept connection
   hSetBuffering handle NoBuffering -- Buffering ??
-  cmd <- parseCmd $ hGetLine handle
-  case cmd of
-    (Left msg)       -> hPutStrLn $ "Invalid command: " ++ msg
-                        handleConns s db
-    (Right Shutdown) -> hPutStrLn "Shutting down server"
-    (Right cmd')     -> handleConn cmd' db
-                        handleConns s db
+  forkIO $ handleConn handle db
+  handleConns s db
 
-handleConn :: Command -> (TVar DB) -> IO ()
-handleConn Shutdown db = return ()
-handleConn cmd db      = 
-  forkIO $ atomically $ handleCmd cmd db
+handleConn :: Handle -> (TVar DB) -> IO ()
+handleConn h db = do
+  cmd <- hGetLine handle
+  res <- atomically $ handleCmd cmd db
+  hPutStrLn h res
+  handleConn h db -- How, what, why!?!
+
