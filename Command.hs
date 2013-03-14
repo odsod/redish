5d1efb1de452bb7a800008d34b9a2a9ca2c4e151
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2013-03-14 11:06:21 +0100

    remove old code in Command.hs

--- Command.hs
+++ Command.hs
@@ -13,14 +13,13 @@ handleCmd cmd tdb = do
 readCommand :: String -> Command
 readCommand = undefined
 
-parseCmd :: String -> Either String Command
-parseCmd []     = Left "No command"
-parseCmd (w:ws) = 
-  case w of
-    "get"      -> if length ws > 0
-                    then return $ Get ws
-                    else Left "Too few arguments to get"
-    "set"      -> if length ws > 1
-                    then return $ Set ws
-                    else Left "Too few arguments to set"
-    _          -> Left $ "Unknown command: " ++ show (w:ws)
+{-parseCmd :: String -> Either String Command-}
+{-parseCmd []     = Left "No command"-}
+{-parseCmd w = case w of-}
+  {-"get" -> if length ws > 0-}
+                  {-then return $ Get ws-}
+                  {-else Left "Too few arguments to get"-}
+  {-"set"      -> if length ws > 1-}
+                  {-then return $ Set ws-}
+                  {-else Left "Too few arguments to set"-}
+  {-_          -> Left $ "Unknown command: " ++ show (w:ws)-}
