91acb34e574343644a0c1f4dcdf9ed47ecba187f
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2013-03-17 13:53:31 +0100

    simplify protocol

--- ConnectionHandler.hs
+++ ConnectionHandler.hs
@@ -11,14 +11,12 @@ newDB = newTVarIO emptyDB
 
 processCommand :: Handle -> TVar RedishDB -> IO ()
 processCommand handle tdb = do
-    numArgs <- ((tail . head . words) <$> hGetLine handle)
-    let numLines = ((read numArgs) :: Int) * 2
-    input <- sequence (replicate numLines (hGetLine handle))
+    input <- hGetLine handle
     reply <- atomically $ handleCommand input tdb
     hPutStr handle reply
     processCommand handle tdb
 
-handleCommand :: [String] -> TVar RedishDB -> STM String
+handleCommand :: String -> TVar RedishDB -> STM String
 handleCommand input tdb = do
   db <- readTVar tdb
   let (rep, db') = interpretCommand db input
--- Redish.hs
+++ Redish.hs
@@ -12,14 +12,14 @@ type RedishCMD = Command String String
 keepOdd :: [a] -> [a]
 keepOdd = snd . unzip . filter (odd . fst) . (zip [0..])
 
-parseCommand :: [String] -> RedishCMD
-parseCommand input = let (c:args) = keepOdd input in case c of
+parseCommand :: String -> RedishCMD
+parseCommand input = let (c:args) = words input in case c of
   "GET" -> Get (args !! 0)
   "SET" -> Set (args !! 0) (args !! 1)
   "DEL" -> Del args
   "APPEND" -> Append (args !! 0) (args !! 1)
 
-interpretCommand :: RedishDB -> [String] -> (String, RedishDB)
+interpretCommand :: RedishDB -> String -> (String, RedishDB)
 interpretCommand db input = 
   let (r, db') = runCommand db (parseCommand input)
   in (show r, db')
